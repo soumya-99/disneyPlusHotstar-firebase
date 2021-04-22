eeb6dea2f951c230997a6a439c5df1399a214165
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-04-22 13:03:14 +0530

    [0.0.3] CTA-logo-1 added

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,9 @@ function Login() {
 	return (
 		<Container>
 			<Content>
+				<CTA>
+					<CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+				</CTA>
 				<BackgroundImage />
 			</Content>
 		</Container>
@@ -43,6 +46,21 @@ const BackgroundImage = styled.div`
 	right: 0;
 	left: 0;
 	z-index: -1;
+`
+
+const CTA = styled.div`
+	max-width: 650px;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+`
+
+const CTALogoOne = styled.img`
+	margin-bottom: 12px;
+	max-width: 600px;
+	min-height: 1px;
+	display: block;
+	width: 100%;
 `
 
 export default Login
