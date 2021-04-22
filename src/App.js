26fbbf5dc8aff0bee8d87db4bf1fe08522754080
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-04-22 20:26:28 +0530

    [0.0.7] All the navigation links with animated bottom done.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import "./App.css"
 import Login from "./components/Login"
+import Navigation from "./components/Navigation"
 
 function App() {
 	return (
 		<div className="App">
 			<Router>
+        <Navigation />
         <Switch>
           <Route exact path="/">
             <Login />
