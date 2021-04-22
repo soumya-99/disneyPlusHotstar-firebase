23c1c8ea846b88c7c601d426d0d7f10ea63f04fa
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-04-22 12:57:37 +0530

    [0.0.2] Background Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import "./App.css"
+import Login from "./components/Login"
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+
+		</div>
+	)
 }
 
-export default App;
+export default App
